15c88fd79fdc700cab1ca53f0be26e094191833a
Yixun-Hu <yixunhu21@gmail.com>
2025-11-30 17:04:33 -0500

    renew the build.sh

---
+++ build_yhu.sh
@@ -0,0 +1,38 @@
+# Build use the /usr camke because the version of conda cmake is too new (4.)
+cd ~/codespace/ORB_SLAM3
+
+# 第三方库 DBoW2
+cd Thirdparty/DBoW2
+rm -rf build
+mkdir build && cd build
+/usr/bin/cmake .. \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_PREFIX_PATH=$CONDA_PREFIX
+make -j4
+
+# g2o
+cd ~/codespace/ORB_SLAM3/Thirdparty/g2o
+rm -rf build
+mkdir build && cd build
+/usr/bin/cmake .. \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_PREFIX_PATH=$CONDA_PREFIX
+make -j4
+
+# Sophus
+cd ~/codespace/ORB_SLAM3/Thirdparty/Sophus
+rm -rf build
+mkdir build && cd build
+/usr/bin/cmake .. \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_PREFIX_PATH=$CONDA_PREFIX
+make -j4
+
+# 主工程 ORB_SLAM3
+cd ~/codespace/ORB_SLAM3
+rm -rf build
+mkdir build && cd build
+/usr/bin/cmake .. \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_PREFIX_PATH=$CONDA_PREFIX
+make -j4
